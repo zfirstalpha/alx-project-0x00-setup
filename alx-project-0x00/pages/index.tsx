7f0a9a72947ebec3ced25716b1afa4b56444909b
zfirstalpha <salim14sadik@gmail.com>
2025-09-30 13:26:05 +0300

    Set up components and interfaces directories

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,9 +1,9 @@
 const Home: React.FC = () => {
   return (
     <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
-      
 
-      <h1>Airbnb Application Clone System</h1>
+
+      <h1>Airbnb Application Clone system</h1>
 
       <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">
         Get Started
